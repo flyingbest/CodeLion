611f5831ed64111ab5a21040bfd24e7acf2f0a89
flyingbest <taeyoon4715@gmail.com>
2017-01-17 07:00:19 +0000

    컨트롤러와 뷰 만들고 라우트 지정하기

--- rails/contact/config/routes.rb
+++ rails/contact/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+	get 'email/email_form' => 'email#email_form'
+	get 'email/email_send' => 'email#email_send'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
